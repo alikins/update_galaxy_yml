4fc8817e4face4505a015b97974e290decef8770
Adrian Likins <alikins@redhat.com>
2019-01-30 12:43:52 -0500

    Mv modules/ or module_utils under plugins/

--- update_collection.sh
+++ update_collection.sh
@@ -1,5 +1,44 @@
 #!/bin/bash -x
 
+ensure_plugins_dir()
+{
+    # if no plugins/ dir, create it, add it, commit it
+    if [ -d plugins/ ]; then
+        return
+    fi
+
+    echo "Adding a plugins/ dir and __init__.py"
+    mkdir plugins/
+    touch plugins/__init__.py
+
+    echo "Adding plugins/ to git"
+    git add plugins
+
+    git commit -m "adding plugins/ dir" plugins
+}
+
+
+# migrate modules/ and module_utils under plugins/
+git ls-files --error-unmatch modules/
+HAS_MODULES=$?
+
+git ls-files --error-unmatch module_utils/
+HAS_MODULE_UTILS=$?
+
+
+# if 'modules/' is tracked by git indicated by ls-files finding it
+if [ $HAS_MODULES -eq 0 ]; then
+    ensure_plugins_dir
+    echo "Moving modules/ dir under plugins/"
+    git mv modules/ plugins/
+fi
+
+if [ $HAS_MODULE_UTILS -eq 0 ]; then
+    ensure_plugins_dir
+    echo "Moving module_utils/ dir under plugins/"
+    git mv module_utils/ plugins/
+fi
+
 (update_galaxy_yml galaxy.yml > galaxy.yml.new)
 cp galaxy.yml.new galaxy.yml
 
